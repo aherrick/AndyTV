b4e38c75cc44ef560cda16380bb2292016eb8636
Andrew Herrick <aj@andrewherrick.com>
2025-09-04 17:56:21 -0400

    Update MenuTVChannelHelper.cs

--- Helpers/Menu/MenuTVChannelHelper.cs
+++ Helpers/Menu/MenuTVChannelHelper.cs
@@ -9,7 +9,7 @@ public class MenuTVChannelHelper(ContextMenuStrip menu)
 
     public async Task LoadChannels(string m3uURL)
     {
-        var parsed = await M3UService.ParseM3U(m3uURL).ConfigureAwait(false);
+        var parsed = await Task.Run(() => M3UService.ParseM3U(m3uURL));
 
         Channels = [.. parsed.OrderBy(c => c.Name, StringComparer.OrdinalIgnoreCase)];
     }
