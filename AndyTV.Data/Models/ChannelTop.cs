ddfa2296ee483e5fcfdb30a971ccb3fb5c536fde
Andrew Herrick <aj@andrewherrick.com>
2025-09-22 19:50:59 -0400

    Refactor channel term matching and remove unused builders
    Introduced a Terms property in ChannelTop to unify access to channel names and alternative names, simplifying term matching logic in MenuTVChannelHelper. Also removed the unused BuildTopUs and BuildTopUk category builder methods to clean up the codebase.

--- AndyTV.Data/Models/ChannelTop.cs
+++ AndyTV.Data/Models/ChannelTop.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AndyTV.Data.Models;
+﻿namespace AndyTV.Data.Models;
 
 public class ChannelTop
 {
     public string Name { get; set; }
     public List<string> AltNames { get; set; }
     public string StreamingTvId { get; set; }
+
+    public IEnumerable<string> Terms
+    {
+        get
+        {
+            var alts = AltNames;
+            if (alts != null)
+            {
+                foreach (var a in alts)
+                {
+                    yield return a;
+                }
+            }
+
+            yield return Name; // Name is always present
+        }
+    }
 }
--- AndyTV/Helpers/Menu/MenuTVChannelHelper.cs
+++ AndyTV/Helpers/Menu/MenuTVChannelHelper.cs
@@ -135,25 +135,34 @@ public partial class MenuTVChannelHelper(ContextMenuStrip menu)
 
             foreach (var entry in entries.OrderBy(e => e.Name, StringComparer.OrdinalIgnoreCase))
             {
-                var terms = new List<string> { entry.Name };
-                if (entry.AltNames != null && entry.AltNames.Count > 0)
-                {
-                    terms.AddRange(entry.AltNames);
-                }
+                var terms = entry.Terms;
 
                 // account for short names (<=2 chars) needing exact match
                 var matches = Channels
                     .Where(ch =>
-                        terms.Any(term =>
-                            term.Length <= 2
-                                ? string.Equals(
-                                    ch.DisplayName,
-                                    term,
-                                    StringComparison.OrdinalIgnoreCase
-                                )
-                                : ch.DisplayName.Contains(term, StringComparison.OrdinalIgnoreCase)
-                        )
-                    )
+                    {
+                        foreach (var term in terms)
+                        {
+                            var isExact = term.Length <= 2;
+                            if (
+                                isExact
+                                    ? string.Equals(
+                                        ch.DisplayName,
+                                        term,
+                                        StringComparison.OrdinalIgnoreCase
+                                    )
+                                    : ch.DisplayName.Contains(
+                                        term,
+                                        StringComparison.OrdinalIgnoreCase
+                                    )
+                            )
+                            {
+                                return true;
+                            }
+                        }
+
+                        return false;
+                    })
                     .OrderBy(c => c.DisplayName, StringComparer.OrdinalIgnoreCase)
                     .ToList();
 
@@ -185,292 +194,5 @@ public partial class MenuTVChannelHelper(ContextMenuStrip menu)
         return Channels.FirstOrDefault(ch =>
             string.Equals(ch.Url.Trim(), url.Trim(), StringComparison.OrdinalIgnoreCase)
         );
-    }
-
-    // ----------------- US/UK category builders -----------------
-
-    private static Dictionary<string, string[][]> BuildTopUs()
-    {
-        return new(StringComparer.OrdinalIgnoreCase)
-        {
-            ["24/7"] =
-            [
-                ["Breaking Bad"],
-                ["Forensic Files"],
-                ["Frasier"],
-                ["Friends"],
-                ["Game of Thrones"],
-                ["I Love Lucy"],
-                ["Saturday Night Live", "SNL"],
-                ["Seinfeld"],
-                ["The Office"],
-                ["The Simpsons"],
-                ["The Sopranos"],
-                ["The Wire"],
-                ["Unsolved Mysteries"],
-            ],
-            ["Entertainment"] =
-            [
-                ["A&E", "AE"],
-                ["AMC"],
-                ["Bravo"],
-                ["Comedy Central"],
-                ["Discovery Channel", "Discovery"],
-                ["Disney Channel", "Disney"],
-                ["E!", "E! Entertainment"],
-                ["Food Network", "Food"],
-                ["FX"],
-                ["FXX"],
-                ["FX Movie Channel", "FXM"],
-                ["FYI"],
-                ["Hallmark Channel", "Hallmark"],
-                ["Hallmark Drama"],
-                ["Hallmark Movies & Mysteries", "Hallmark Movies and Mysteries", "HMM"],
-                ["HGTV"],
-                ["History", "History Channel"],
-                ["IFC"],
-                ["Lifetime"],
-                ["National Geographic", "Nat Geo"],
-                ["National Geographic Wild", "Nat Geo Wild"],
-                ["Oxygen"],
-                ["Paramount Network", "Paramount"],
-                ["Smithsonian Channel", "Smithsonian"],
-                ["Syfy", "Sci-Fi"],
-                ["TBS"],
-                ["TCM", "Turner Classic Movies"],
-                ["TNT"],
-                ["Travel Channel"],
-                ["TruTV"],
-                ["USA Network"],
-                ["VH1"],
-                ["WE TV"],
-                ["Crime + Investigation", "Crime & Investigation", "Crime and Investigation"],
-            ],
-            ["Kids"] =
-            [
-                ["Boomerang"],
-                ["Cartoon Network", "CN"],
-                ["Disney Junior"],
-                ["Disney XD"],
-                ["Nick Jr", "Nick Junior"],
-                ["Nickelodeon", "Nick"],
-                ["Nicktoons"],
-                ["Universal Kids"],
-            ],
-            ["Locals"] =
-            [
-                ["ABC"],
-                ["Antenna TV", "Antenna"],
-                ["CBS"],
-                ["Cozi TV", "Cozi"],
-                ["CW"],
-                ["FOX"],
-                ["Ion Mystery", "ION Mystery", "Ion Plus Mystery"],
-                ["MeTV"],
-                ["MyNetworkTV", "MyNetwork TV", "MyTV"],
-                ["NBC"],
-                ["PBS"],
-            ],
-            ["Movies"] =
-            [
-                ["5StarMax"],
-                ["ActionMax"],
-                ["Cinemax"],
-                ["Epix", "MGM+"],
-                ["Epix 2", "MGM+ 2"],
-                ["Epix Drive-In", "MGM+ Drive-In", "MGM+ Drive In"],
-                ["Epix Hits", "MGM+ Hits"],
-                ["Flix"],
-                ["HBO"],
-                ["HBO 2"],
-                ["HBO Comedy"],
-                ["HBO Family"],
-                ["HBO Signature"],
-                ["HBO Zone"],
-                ["MoreMax"],
-                ["Showtime"],
-                ["Showtime 2"],
-                ["Showtime Extreme"],
-                ["Showtime Showcase"],
-                ["Starz"],
-                ["Starz Cinema"],
-                ["Starz Comedy"],
-                ["Starz Edge"],
-                ["Starz Kids & Family", "Starz Kids and Family"],
-                ["The Movie Channel", "TMC"],
-                ["The Movie Channel Xtra", "TMC Xtra", "TMCXtra"],
-            ],
-            ["Music"] =
-            [
-                ["AXS TV", "AXS"],
-                ["BET"],
-                ["CMT"],
-                ["MTV"],
-                ["MTV2"],
-                ["Music Choice"],
-            ],
-            ["News"] =
-            [
-                ["ABC News"],
-                ["Bloomberg"],
-                ["CBS News"],
-                ["CNBC"],
-                ["CNN"],
-                ["CSPAN", "C-SPAN"],
-                ["CSPAN 2", "C-SPAN 2"],
-                ["Fox Business", "Fox Business Network"],
-                ["Fox News", "Fox News Channel"],
-                ["HLN", "Headline News"],
-                ["MSNBC"],
-                ["NBC News"],
-                ["NewsNation"],
-                ["Newsmax"],
-                ["OANN", "One America News", "One America News Network"],
-                ["The Weather Channel", "Weather Channel"],
-            ],
-            ["Other"] =
-            [
-                ["BBC America"],
-                ["Court TV", "CourtTV"],
-                ["GSN", "Game Show Network"],
-                ["ION", "ION Television"],
-                ["ION Plus", "IonPlus"],
-                ["Reelz"],
-                ["Trinity Broadcasting", "TBN", "Trinity Broadcasting Network"],
-            ],
-            ["Sports"] =
-            [
-                ["ACC Network", "ACCN"],
-                ["Big Ten Network", "BTN"],
-                ["BTN"],
-                ["CBS Sports Network", "CBSSN"],
-                ["ESPN"],
-                ["ESPN 2", "ESPN2"],
-                ["ESPN News", "ESPNews"],
-                ["ESPNU"],
-                ["Fox Sports 1", "FS1"],
-                ["Fox Sports 2", "FS2"],
-                ["Golf Channel"],
-                ["MLB Network"],
-                ["NBA TV", "NBATV"],
-                ["NFL Network"],
-                ["NFL RedZone", "RedZone"],
-                ["NHL Network"],
-                ["SEC Network", "SECN"],
-            ],
-        };
-    }
-
-    private static Dictionary<string, string[][]> BuildTopUk()
-    {
-        return new(StringComparer.OrdinalIgnoreCase)
-        {
-            ["Documentary"] =
-            [
-                ["Crime+Investigation", "Crime & Investigation", "Crime and Investigation"],
-                ["Discovery History"],
-                ["Discovery Science"],
-                ["Sky History", "History (UK)"],
-            ],
-            ["Entertainment"] =
-            [
-                ["Alibi"],
-                ["BBC Alba"],
-                ["BritBox"],
-                ["Dave"],
-                ["Drama"],
-                ["Eden"],
-                ["Gold"],
-                ["Pick"],
-                ["Quest"],
-                ["Really"],
-                ["Sky Atlantic"],
-                ["Sky Max"],
-                ["Sky Mix"],
-                ["Sky Showcase"],
-                ["Sky Witness"],
-                ["W"],
-                ["Yesterday"],
-            ],
-            ["Kids"] =
-            [
-                ["CBBC"],
-                ["CBeebies"],
-                ["Cartoonito"],
-                ["POP"],
-                ["Tiny Pop"],
-            ],
-            ["Main"] =
-            [
-                ["4seven"],
-                ["5Action"],
-                ["5Select"],
-                ["5Star"],
-                ["5USA"],
-                ["BBC Four"],
-                ["BBC One"],
-                ["BBC Scotland"],
-                ["BBC Three"],
-                ["BBC Two"],
-                ["Channel 4"],
-                ["Channel 5"],
-                ["E4"],
-                ["ITV1"],
-                ["ITV2"],
-                ["ITV3"],
-                ["ITV4"],
-                ["ITVBe"],
-                ["S4C"],
-                ["STV"],
-                ["UTV"],
-            ],
-            ["Movies"] =
-            [
-                ["Film4"],
-                ["Sky Cinema Action"],
-                ["Sky Cinema Animation"],
-                ["Sky Cinema Comedy"],
-                ["Sky Cinema Drama"],
-                ["Sky Cinema Family"],
-                ["Sky Cinema Greats"],
-                ["Sky Cinema Premiere"],
-                ["Sky Cinema Sci-Fi & Horror", "Sky Cinema Sci-Fi and Horror"],
-                ["Sky Cinema Thriller"],
-            ],
-            ["Music"] =
-            [
-                ["4Music"],
-                ["Kerrang!"],
-                ["Kiss TV"],
-                ["The Box"],
-            ],
-            ["News"] =
-            [
-                ["Al Jazeera English", "Al Jazeera"],
-                ["BBC News"],
-                ["BBC Parliament"],
-                ["Euronews"],
-                ["France 24 English", "France 24"],
-                ["GB News"],
-                ["Sky News"],
-                ["TalkTV"],
-            ],
-            ["Sports"] =
-            [
-                ["Eurosport 1"],
-                ["Eurosport 2"],
-                ["Premier Sports"],
-                ["Sky Sports Cricket"],
-                ["Sky Sports F1"],
-                ["Sky Sports Football"],
-                ["Sky Sports Golf"],
-                ["Sky Sports Main Event"],
-                ["Sky Sports News"],
-                ["TNT Sports 1"],
-                ["TNT Sports 2"],
-                ["TNT Sports 3"],
-                ["TNT Sports 4"],
-            ],
-        };
     }
 }
