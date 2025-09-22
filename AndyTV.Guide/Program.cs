c02d529bfbcefdc736e9bfe9eeb21fdfa85bea8a
Andrew Herrick <aj@andrewherrick.com>
2025-09-22 11:16:56 -0400

    init of guide

---
+++ AndyTV.Guide/Program.cs
@@ -0,0 +1,17 @@
+using AndyTV.Guide.Pages;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+SyncfusionLicenseProvider.RegisterLicense(
+    "Ngo9BigBOggjHTQxAR8/V1JFaF5cXGRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXZccnVVR2ldVE1/W0tWYEg="
+);
+
+builder.Services.AddSyncfusionBlazor();
+
+// Render GuidePage (or whatever your main component is) directly
+builder.RootComponents.Add<Guide>("#app");
+
+await builder.Build().RunAsync();
