486603341dc6c1ee534cfef18577de68178c9e38
Andrew Herrick <aj@andrewherrick.com>
2025-10-19 09:14:09 -0400

    Update Program.cs

--- AndyTV.Guide/Program.cs
+++ AndyTV.Guide/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
 using Syncfusion.Licensing;
+using AndyTV.Guide.Pages;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
