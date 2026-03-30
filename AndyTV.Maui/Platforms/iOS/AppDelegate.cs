31e18085467b82678089736635fb35b8abf81a29
Andrew Herrick <aj@andrewherrick.com>
2026-03-30 15:27:50 -0400

    Activate AVAudioSession for iOS playback
    Add AVFoundation import and override FinishedLaunching to set AVAudioSession category to Playback and activate the audio session. Ensures the app's audio playback is configured and active on iOS at launch.

--- AndyTV.Maui/Platforms/iOS/AppDelegate.cs
+++ AndyTV.Maui/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,5 @@
-﻿using Foundation;
+﻿using AVFoundation;
+using Foundation;
 using UIKit;
 
 namespace AndyTV.Maui;
@@ -7,4 +8,12 @@ namespace AndyTV.Maui;
 public class AppDelegate : MauiUIApplicationDelegate
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        AVAudioSession.SharedInstance().SetCategory(AVAudioSessionCategory.Playback);
+        AVAudioSession.SharedInstance().SetActive(true);
+
+        return base.FinishedLaunching(application, launchOptions);
+    }
 }
