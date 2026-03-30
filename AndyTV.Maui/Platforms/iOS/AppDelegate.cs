83a9364260953535f00f0944dbb1450b1edf84f6
Andrew Herrick <aj@andrewherrick.com>
2026-03-30 15:17:34 -0400

    Remove LibVLC and iOS audio session setup
    Remove LibVLCSharp.MAUI references from MauiProgram (deleted using and .UseLibVLCSharp()). Remove iOS AVAudioSession initialization and FinishedLaunching override from AppDelegate and drop the AVFoundation using. Clean up unused/useless platform-specific audio setup.

--- AndyTV.Maui/Platforms/iOS/AppDelegate.cs
+++ AndyTV.Maui/Platforms/iOS/AppDelegate.cs
@@ -1,5 +1,4 @@
-﻿using AVFoundation;
-using Foundation;
+﻿using Foundation;
 using UIKit;
 
 namespace AndyTV.Maui;
@@ -8,12 +7,4 @@ namespace AndyTV.Maui;
 public class AppDelegate : MauiUIApplicationDelegate
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-
-    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
-    {
-        AVAudioSession.SharedInstance().SetCategory(AVAudioSessionCategory.Playback);
-        AVAudioSession.SharedInstance().SetActive(true);
-
-        return base.FinishedLaunching(application, launchOptions);
-    }
 }
