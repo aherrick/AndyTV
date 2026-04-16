393c1317411da86d2aa7f18c173c889933eec4d8
Andrew Herrick <aj@andrewherrick.com>
2026-04-16 15:49:23 -0400

    Use file-scoped namespaces for platform files
    Convert Android, iOS and MacCatalyst platform files to file-scoped namespaces and simplify their entry classes: collapse the nested MainActivity declaration and apply the [Activity] attribute directly; make Program a static class and clean up the Main method in iOS/MacCatalyst. Also add a comment above the LibVLCSharp.MAUI package reference (linking an upstream issue) in the project file.

--- AndyTV.Maui/Platforms/Android/MainActivity.cs
+++ AndyTV.Maui/Platforms/Android/MainActivity.cs
@@ -2,10 +2,9 @@
 using Android.Content.PM;
 using Android.OS;
 
-namespace AndyTV.Maui
+namespace AndyTV.Maui;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
-    {
-    }
 }
